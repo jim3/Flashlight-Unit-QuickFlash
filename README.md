f163d2bdd13998d2d46cdb5410b7aff0e4e708d5
jim3 <jimmy3.xyz@gmail.com>
2024-03-18 17:28:05 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+### M5Stack Flashlight Unit
+
+The device has built-in flash, including AW3641 driver and a white LED, with a color temperature of 5000-5700K
+The "white LED" really does have that nice "_camera flash effect_", totally worth the price of this device, and
+turns are to be _perfect_ for an alert light that's triggered by an _event_ or _alarm_.
+
+The communication interface is GPIO. I tested this on the Arduino Uno R4 WiFi and it works well. The connection is as follows:
+
+M5Stack -> Arduino Uno R4 WiFi
+
+-   5V -> 5V
+-   GND -> GND
+-   S -> GPIO 9
+
+The LED is turned on when pin is `HIGH` and turned off when the GPIO 9 pin is `LOW`.
---
+++ flashlight-unit.cpp
@@ -0,0 +1,23 @@
+
+
+#include <Serial.h>
+
+const int flashlightPin = 9;  // D9 GPIO
+
+void setup() {
+    Serial.begin(9600);              // Start serial communication at 9600 baud rate
+    pinMode(flashlightPin, OUTPUT);  // Set the control pin as output
+    Serial.println("Starting test program...");
+}
+
+// Quick flash, used for alerts
+void loop() {
+    Serial.println("Turning on LED...");
+    digitalWrite(flashlightPin, HIGH);
+    delay(1);  // Wait for 1ms second
+    Serial.println("Turning off LED...");
+    digitalWrite(flashlightPin, LOW);
+    delay(5000);  // Wait for 5000ms
+}
+
+// vIt's a 5V device, so I used a 1K resistor to limit the current to 5mA
